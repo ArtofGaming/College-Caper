0abadbee7096c38860fb8c1c73b606caadb5f58f
ArtofGaming <turner4965@gmail.com>
2024-04-20 15:15:40 -0700

    added item filtering

--- Assets/Web.cs
+++ Assets/Web.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Web : MonoBehaviour
 {
@@ -15,6 +16,10 @@ public class Web : MonoBehaviour
     {
         StartCoroutine(GetItemIDs(Main.Instance.UserInfo.UserID));
     }*/
+    public void Filter(Button button)
+    {
+        StartCoroutine(FilterItems(button.name));
+    }
 
     public IEnumerator GetItemIDs()
     {
@@ -43,6 +48,29 @@ public class Web : MonoBehaviour
         form.AddField("itemID", itemID);
 
         using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/FashionGame/GetItem.php", form))
+        {
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                string jsonArray = www.downloadHandler.text;
+
+            }
+        }
+    }
+
+    public IEnumerator FilterItems(string filter)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("filter", filter);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/FashionGame/FilterItems.php", form))
         {
 
             yield return www.SendWebRequest();
